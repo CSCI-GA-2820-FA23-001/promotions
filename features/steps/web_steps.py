4cf6f813b47d1ad7fbfc1f96a77c53bc984b366e
Zhe Ye <113217365+hiddenyz@users.noreply.github.com>
2023-12-10 21:35:29 -0500

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -153,7 +153,7 @@ def step_impl(context, button):
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
     extended_wait = 90
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
+    found = WebDriverWait(context.driver, extended_wait).until(
         expected_conditions.text_to_be_present_in_element(
             (By.ID, "search_results"), name
         )
